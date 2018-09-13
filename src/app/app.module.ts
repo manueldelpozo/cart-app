cd933a5594640f2eadeef4d099436b30a039c4d6
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2018-09-13 17:54:21 +0200

    Generate component, models and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { CartListComponent } from './components/cart-list/cart-list.component';
+import { CartItemComponent } from './components/cart-item/cart-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductCardComponent,
+    CartListComponent,
+    CartItemComponent
   ],
   imports: [
     BrowserModule
